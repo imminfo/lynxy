1af505def0cbef946abc19de00e58f6586b1800c
Vadim Nazarov <vdn@mailbox.org>
2016-02-16 16:22:12 +0300

    add python package skeleton

---
+++ lynxyPy/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lynxy',
+      version='0.1',
+      description='blah-blah',
+      url='http://github.com/imminfo/lynxy',
+      author='Flying Circus',
+      author_email='vdm.nazarov@gmail.com',
+      license='Apache v2',
+      packages=['lynxy'],
+      zip_safe=False)
